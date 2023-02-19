d55574a60dcd0ff24e55b4e71cb224bfbb11b1e4
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2023-02-19 14:09:22 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,3 +1,75 @@
+# With a big tip of the hat to Ali for rewriting and optimizing my crappy code!
+
+import numpy as np
+
+class Game2048:
+    def __init__(self):
+        self.board = np.zeros((4, 4), dtype=np.uint16)
+        self.score = 0
+        self.add_new_tile()
+        self.add_new_tile()
+
+    def slide(self, row: np.ndarray, right: bool = False) -> np.ndarray:
+        if right:
+            row = row[::-1]
+        merged = False
+        for i in range(len(row)-1):
+            if row[i] == row[i+1]:
+                self.score += row[i] * 2
+                row[i] <<= 1
+                row[i+1] = 0
+                merged = True
+        row = row[row != 0]
+        row = np.concatenate((row, np.zeros(len(self.board) - len(row), dtype=np.uint16)))
+        if right:
+            row = row[::-1]
+        return row, merged
+
+    def add_new_tile(self):
+        empty_cells = np.where(self.board == 0)
+        if len(empty_cells[0]) == 0:
+            return False
+        i = np.random.randint(len(empty_cells[0]))
+        value = np.random.choice([2, 4], p=[0.75, 0.25])
+        self.board[empty_cells[0][i], empty_cells[1][i]] = value
+        return True
+
+    def play_move(self, move: int):
+        prev_board = self.board.copy()
+        if move == 0:
+            self.board = self.board.T
+            for n, i in enumerate(self.board):
+                self.board[n], merged = self.slide(i)
+            self.board = self.board.T
+        elif move == 1:
+            for n, i in enumerate(self.board):
+                self.board[n], merged = self.slide(i, True)
+        elif move == 2:
+            self.board = self.board.T
+            for n, i in enumerate(self.board):
+                self.board[n], merged = self.slide(i, True)
+            self.board = self.board.T
+        elif move == 3:
+            for n, i in enumerate(self.board):
+                self.board[n], merged = self.slide(i)
+        else:
+            return False
+        if not np.array_equal(self.board, prev_board):
+            if not self.add_new_tile():
+                print(f"Game over. Score: {self.score}")
+        return True
+
+    def play_game(self):
+        while True:
+            print(self.board)
+            move = int(input(f"Score: {self.score} Your move (0: up, 1: right, 2: down, 3: left): "))
+            if not self.play_move(move):
+                print("Invalid move. Please try again.")
+
+game = Game2048()
+game.play_game()
+
+'''
 import random
 
 board = [
@@ -59,3 +131,4 @@ while True:
             if board[new_y][new_x] == 0:
                 board[new_y][new_x] = 2 if random.random() < 0.75 else 4
                 new_tile = True
+'''
